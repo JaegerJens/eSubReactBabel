16217d0cfb713f5d14ea8042aa163347d6f17eb1
Jens Jaeger <jaegerj204@googlemail.com>
2016-12-19 14:09:12 +0100

    add webpack-dev-server and use CSS grid for layout

--- src/components/mainview/mainview.jsx
+++ src/components/mainview/mainview.jsx
@@ -5,10 +5,10 @@ require('./mainview.css');
 export default class MainView extends React.Component {
     render() {
         return <div className="mainview-layout">
-            <div>Hierarchy View</div>
-            <div>Sequence View</div>
-            <div>Document View</div>
-            <div>Attribute View</div>
+            <div className="hierarchy-container">Hierarchy View</div>
+            <div className="sequence-container">Sequence View</div>
+            <div className="document-container">Document View</div>
+            <div className="attribute-container">Attribute View</div>
         </div>
     }
 }
