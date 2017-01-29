a74650f91bc6f734fe3e028a93848eebd93bec53
Jens Jaeger <jaegerj204@googlemail.com>
2017-01-29 10:50:46 +0100

    export and test ListItem of Tree component

--- src/components/tree/tree.jsx
+++ src/components/tree/tree.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 require('./tree.css');
 
-const ListItem = ({number, value, onClick, isSelected}) => {
+export const ListItem = ({number, value, onClick, isSelected}) => {
     const onClickHandler = () => onClick(number, value);
     if (isSelected) {
         return <li className="selected" onClick={onClickHandler}>{value}</li>;
--- src/components/tree/tree.test.js
+++ src/components/tree/tree.test.js
@@ -1,5 +1,5 @@
 import React from 'React';
-import Tree from './tree.jsx';
+import Tree, {ListItem} from './tree.jsx';
 import renderer from 'react-test-renderer';
 
 describe('Tree component', () => {
@@ -10,5 +10,26 @@ describe('Tree component', () => {
             onClick: 'onClick function'
         };
         expect(Tree(props)).toMatchSnapshot();
+    });
+});
+
+describe('ListItem component', () => {
+    it('matches selected snapshot', () => {
+        const props = {
+            number: 3,
+            value: 'id5424',
+            onClick: () => console.log('click'),
+            isSelected: true
+        };
+        expect(ListItem(props)).toMatchSnapshot();
+    });
+    it('matches unselected snapshot', () => {
+        const props = {
+            number: 4,
+            value: 'id3424',
+            onClick: () => console.log('click'),
+            isSelected: false
+        };
+        expect(ListItem(props)).toMatchSnapshot();
     });
 });
